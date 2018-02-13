9e8231554ce7eb0c438d34d468ec709c05af85b7
jackokello <jack.okello@gmail.com>
2018-02-13 20:23:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
-function logshout(string){
-  console.log("toUpperCase")
+function logShout(string){
+  console.log("string.toUpperCase()")
 }
